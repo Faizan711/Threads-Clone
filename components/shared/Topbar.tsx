9eb0d28eec260f6733857b99e24e8a83260cd9a2
Faizan <faizanalam980@gmail.com>
2023-08-28 19:10:53 +0530

    completed Topbar

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,5 +1,39 @@
+import { OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+
 function Topbar(){
-    return <h1>Topbar</h1>
+    return (
+        <nav className="topbar">
+            <Link href='/' className="flex items-center gap-4">
+                <Image src='/assets/logo.svg' alt="logo" width={28} height={28}/>
+                <p className="text-heading3-bold text-light-1 max-xs-hidden">
+                    Threads
+                </p>
+            </Link>
+
+            <div className="flex items-center gap-1">
+                <div className="block md:hidden">
+                {/* OnlyShown to loggedin users  */}
+                    <SignedIn> 
+                        <SignOutButton>
+                            <div className="flex cursor-pointer">
+                                <Image src='/assets/logout.svg' alt="logout" width={24} height={24}/>
+                            </div>
+                        </SignOutButton>
+                    </SignedIn>
+                </div>
+            </div>
+            {/* Component for Organization By Clerk  */}
+            <OrganizationSwitcher
+                appearance={{
+                    elements:{
+                        organizationSwitcherTrigger: "py-2 px-4"
+                    }
+                }}
+            />
+        </nav>
+    )
 }
 
 export default Topbar;
