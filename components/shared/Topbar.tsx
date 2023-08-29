665d12ce00ce2b416f77d20fab139c9ea2e3a80b
Faizan <faizanalam980@gmail.com>
2023-08-29 16:15:01 +0530

    added dark theme to organizationswitcher

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -1,6 +1,7 @@
 import { OrganizationSwitcher, SignOutButton, SignedIn } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
+import { dark } from '@clerk/themes'
 
 function Topbar(){
     return (
@@ -27,6 +28,7 @@ function Topbar(){
             {/* Component for Organization By Clerk  */}
             <OrganizationSwitcher
                 appearance={{
+                    baseTheme: dark,
                     elements:{
                         organizationSwitcherTrigger: "py-2 px-4"
                     }
