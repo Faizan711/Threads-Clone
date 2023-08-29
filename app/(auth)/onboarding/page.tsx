4bd3f12a4e97d289b7a98c2e1b9f46731d4d671a
Faizan <faizanalam980@gmail.com>
2023-08-29 20:03:40 +0530

    imported and created till onboarding form

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,7 +1,14 @@
+import AccountProfile from "@/components/forms/AccountProfile";
+
 async function Page(){
     return(
-        <main>
-            <h1 className="head-text">Onboarding page</h1>
+        <main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
+            <h1 className="head-text">Onboarding</h1>
+            <p className="mt-3 text-base-regular text-light-2">Complete your Profile now to use Threads</p>
+
+            <section className="mt-9 bg-dark-2 p-10">
+                <AccountProfile/>
+            </section>
         </main>
     )
 }
