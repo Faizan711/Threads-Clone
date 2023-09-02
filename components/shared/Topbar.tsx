b7de9f0c5ed1bf420341a1b2e14722d60dc2005e
Faizan <faizanalam980@gmail.com>
2023-09-02 16:20:29 +0530

    minor fixes

--- components/shared/Topbar.tsx
+++ components/shared/Topbar.tsx
@@ -8,7 +8,7 @@ function Topbar(){
         <nav className="topbar">
             <Link href='/' className="flex items-center gap-4">
                 <Image src='/assets/logo.svg' alt="logo" width={28} height={28}/>
-                <p className="text-heading3-bold text-light-1 max-xs-hidden">
+                <p className="text-heading3-bold text-light-1 max-xs:hidden">
                     Threads
                 </p>
             </Link>
@@ -30,7 +30,7 @@ function Topbar(){
                 appearance={{
                     baseTheme: dark,
                     elements:{
-                        organizationSwitcherTrigger: "py-2 px-4"
+                        organizationSwitcherTrigger: "py-2"
                     }
                 }}
             />
