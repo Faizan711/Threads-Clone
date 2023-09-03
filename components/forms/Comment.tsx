19a3a13e12308d1f74cbadf7434f4bfff2c1b58f
Faizan <faizanalam980@gmail.com>
2023-09-03 17:15:44 +0530

    comment section component

---
+++ components/forms/Comment.tsx
@@ -0,0 +1,91 @@
+'use client'
+
+import * as z from "zod"
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Input } from "../ui/input";
+import { usePathname, useRouter } from "next/navigation";
+import { commentValidation } from "@/lib/validations/thread";
+// import { createThread } from "@/lib/actions/thread.actions";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import Image from "next/image";
+import User from "@/lib/models/user.model";
+
+interface Props {
+    threadId : string;
+    currentUserImg : string;
+    currentUserId : string;
+}
+
+const Comment = ({
+    threadId,
+    currentUserImg,
+    currentUserId
+} : Props) => {
+
+    const router = useRouter();
+    const pathname  = usePathname();
+
+    const form = useForm({
+        resolver: zodResolver(commentValidation),
+        defaultValues: {
+            thread: ''
+        }
+    });
+
+    const onSubmit = async (values : z.infer<typeof commentValidation>) => {
+        // await createThread({
+        //     text : values.thread,
+        //     author : userId,
+        //     communityId : null,
+        //     path : pathname
+        // }); 
+
+        router.push('/');
+    }
+
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="comment-form ">
+                <FormField
+                control={form.control}
+                name="thread"
+                render={({ field }) => (
+                <FormItem className="flex w-full items-center gap-3 ">
+                    <FormLabel>
+                        <Image
+                        src={currentUserImg}
+                        alt="profile photo"
+                        width={48}
+                        height={48}
+                        className="rounded-full object-cover"
+                        />
+                    </FormLabel>
+                    <FormControl className="border-none bg-transparent">
+                    <Input 
+                        type="text"
+                        placeholder="Comment ..."
+                        className="no-focus text-light-1 outline-none"
+                        {...field}
+                    />
+                    </FormControl>
+                </FormItem>
+                )}
+                />
+                <Button type="submit" className="comment-form_btn">
+                    Reply
+                </Button>
+            </form>
+        </Form>
+    )
+}
+
+export default Comment;
