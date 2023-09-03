efec28c12fbba4024da71384087fb865d0ebdac9
Faizan <faizanalam980@gmail.com>
2023-09-03 16:37:27 +0530

    completed fetchThreadById function except comunity

--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -66,3 +66,42 @@ export async function fetchPosts(pageNumber = 1, pageSize = 20){
 
     return { posts, isNext };
 }
+
+export async function fetchThreadById(id: string){
+    connectToDb();
+
+    try {
+
+        //TODO: populate the community later too
+        const thread = Thread.findById(id)
+        .populate({
+            path: 'author',
+            model: User,
+            select: '_id id name image'
+        })
+        .populate({
+            path: 'children',
+            populate: [
+                {
+                    path: 'author',
+                    model: User,
+                    select: '_id id name parentId image'
+                },
+                {
+                    path: 'children',
+                    model: Thread,
+                    populate: {
+                        path: 'author',
+                        model: User,
+                        select: "_id id name parentId image"
+                    }
+                }
+            ]
+        }).exec();
+
+        return thread;
+
+    } catch (error : any) {
+        throw new Error(`Failed to fetch the Thread : ${error.message}`);
+    }
+}
