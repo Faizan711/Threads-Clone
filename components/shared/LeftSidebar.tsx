ed944f3e8560a12494cf21c6ac32b54380130de4
Faizan <faizanalam980@gmail.com>
2023-09-04 17:26:18 +0530

    fixed profile route

--- components/shared/LeftSidebar.tsx
+++ components/shared/LeftSidebar.tsx
@@ -2,7 +2,7 @@
 
 import {sidebarLinks} from '@/constants'
 import { SignedIn } from '@clerk/clerk-react';
-import { SignOutButton } from '@clerk/nextjs';
+import { SignOutButton, useAuth } from '@clerk/nextjs';
 import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname, useRouter } from 'next/navigation'
@@ -11,13 +11,16 @@ function LeftSidebar(){
 
     const router = useRouter();
     const pathname = usePathname();
+    const { userId } = useAuth();
 
     return(
         <section className="custom-scrollbar leftsidebar">
             <div className="flex w-full flex-1 flex-col gap-6 px-6">
                 {sidebarLinks.map((link) => {
 
-                const isActive = (pathname.includes(link.route) && link.route.length > 1) || pathname === link.route;                   
+                const isActive = (pathname.includes(link.route) && link.route.length > 1) || pathname === link.route;     
+                
+                if(link.route === '/profile') link.route =  `${link.route}/${userId}`
 
                 return (
                     <div>
