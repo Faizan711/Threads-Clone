704f9f724597d273faa528f6f184c9c4d2cecc99
Faizan <faizanalam980@gmail.com>
2023-09-05 16:43:22 +0530

    search page completed except searchbar

--- app/(root)/search/page.tsx
+++ app/(root)/search/page.tsx
@@ -1,4 +1,5 @@
-import { fetchUser } from "@/lib/actions/user.actions";
+import UserCard from "@/components/cards/UserCard";
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
@@ -12,9 +13,39 @@ const Page = async () => {
     const userInfo = await fetchUser(user.id); 
     if(!userInfo?.onboarding) redirect('/onboarding'); //to check if onboarding is done or not
 
+    //Fetch Users
+    const result = await fetchUsers({
+        userId: user.id,
+        searchString: "",
+        pageNumber: 1,
+        pageSize: 25,
+        sortBy: 'desc'
+    })
+
     return(
         <section>
             <h1 className="head-text mb-10">Search</h1>
+
+            {/* search bar  */}
+
+            <div className="mt-14 flex flex-col gap-9">
+                {result.users.length === 0 ? (
+                    <p className="no-result">No Users</p>
+                ) : (
+                    <>
+                        {result.users.map((person) => (
+                            <UserCard 
+                            key={person.id}
+                            id={person.id}
+                            name={person.name}
+                            username={person.username}
+                            imageUrl={person.image}
+                            personType='User'
+                            />
+                        ))}
+                    </>
+                )}  
+            </div>
         </section>
     )
 }
