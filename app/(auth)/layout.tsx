870e77671ed92a6863d338f85530be7ebc42f513
Faizan <faizanalam980@gmail.com>
2023-09-05 17:12:30 +0530

    fixed layout for signup & signin page

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -17,7 +17,9 @@ export default function RootLayout({children} : {children: React.ReactNode}){
         <ClerkProvider>
             <html lang='en'>
                 <body className={`${inter.className} bg-dark-1`}>
-                    {children}
+                    <div className='w-full flex justify-center items-center min-h-screen'>
+                        {children}
+                    </div>
                 </body>
             </html>
         </ClerkProvider>
