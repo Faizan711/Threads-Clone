ac07463c49229fb3dd557bbcdccbc00f70c0fd3a
Faizan <faizanalam980@gmail.com>
2023-09-06 16:39:57 +0530

    community page completed

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -1,9 +1,54 @@
+import CommunityCard from "@/components/cards/CommunityCard";
+import UserCard from "@/components/cards/UserCard";
+import { fetchCommunities } from "@/lib/actions/community.actions";
+import { fetchUser, fetchUsers } from "@/lib/actions/user.actions";
+import Community from "@/lib/models/community.model";
+import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
 
 const Page = async () => {
+
+        
+    const user = await currentUser();
+    if(!user) return null;
+
+    const userInfo = await fetchUser(user.id); 
+    if(!userInfo?.onboarding) redirect('/onboarding'); //to check if onboarding is done or not
+
+    //Fetch communities
+    const result = await fetchCommunities({
+        searchString: "",
+        pageNumber: 1,
+        pageSize: 25,
+        sortBy: 'desc'
+    })
+
     return(
         <section>
-            <h1 className="head-text mb-10">Communities</h1>
+            <h1 className="head-text mb-10">Search</h1>
+
+            {/* search bar  */}
+
+            <div className="mt-14 flex flex-col gap-9">
+                {result.communities.length === 0 ? (
+                    <p className="no-result">No communities</p>
+                ) : (
+                    <>
+                        {result.communities.map((community) => (
+                            <CommunityCard 
+                            key={community.id}
+                            id={community.id}
+                            name={community.name}
+                            username={community.username}
+                            imgUrl={community.image}
+                            bio={community.bio}
+                            members={community.members}
+                            />
+                        ))}
+                    </>
+                )}  
+            </div>
         </section>
     )
 }
